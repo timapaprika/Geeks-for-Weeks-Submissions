cb2b2a4aff93fce18f977e6081c3fca9c3503ab1
timapaprika <108842907+timapaprika@users.noreply.github.com>
2022-07-21 20:58:29 -0400

    submissions 1-3

---
+++ day1.py
@@ -0,0 +1,8 @@
+print('hello world')
+print('ur mom')
+num = 33
+print(num)
+if num > 20: 
+    print(num, 'is a positive number')
+    else:
+        print(num 'is dumb')
---
+++ day2.py
@@ -0,0 +1,38 @@
+one = 1 #variables
+two = 2
+print(two + one)
+three = one + two
+
+x = 'hello '
+y = 'world'
+print(x + y)
+
+print(one + two)
+print(x + y)
+print(three)
+
+varOne = True
+varTwo = False
+print(varOne)
+print(varTwo) #variables 
+
+if three == 3: #conditionals
+    print('ye')
+
+print(three)
+print(varOne)
+
+if varTwo == True:
+    print('varTwo is true')
+
+if (False):
+    print('varOne is false') #conditionals
+
+#loops
+for ak in range(10):
+    print('looped', two)
+
+for ak in range(10):
+    print(ak)
+
+print(range(10))
